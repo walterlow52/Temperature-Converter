2619d49203fb38da446ab1da083052280ae0e6e4
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-04-13 17:22:25 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 function convertTemp(degrees) {
   var x;
   if (degrees = 'C') {
-    x = (document.getElementById("celsius").value * 9) / 5 + 32;
-    document.getElementById("farenheit").value = Math.round(x);
+    x = (document.getElementByClass("celsius").value * 9) / 5 + 32;
+    document.getElementByClass("farenheit").value = Math.round(x);
   }
   
   if (degrees = 'F') {
-    x = ((document.getElementById("farenheit").value - 32) * 5) / 9; 
-    document.getElementById("celsius").value = Math.round(x);
+    x = ((document.getElementByClass("farenheit").value - 32) * 5) / 9; 
+    document.getElementByClass("celsius").value = Math.round(x);
   }
 }
